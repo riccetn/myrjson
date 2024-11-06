c260a4cfadcac92a1b294bb4276a303991013469
Rickard Närström <rickard@narstrom.se>
2024-11-06 21:19:27 +0100

    Simplify

--- src/test/java/se/narstrom/myr/json/parser/MyrJsonStreamParserTest.java
+++ src/test/java/se/narstrom/myr/json/parser/MyrJsonStreamParserTest.java
@@ -2,42 +2,35 @@ package se.narstrom.myr.json.parser;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import java.io.Reader;
 import java.io.StringReader;
-import java.util.Map;
 
 import org.junit.jupiter.api.Test;
 
 import com.code_intelligence.jazzer.api.FuzzedDataProvider;
 import com.code_intelligence.jazzer.junit.FuzzTest;
 
+import jakarta.json.Json;
 import jakarta.json.JsonException;
+import jakarta.json.stream.JsonParser;
 import jakarta.json.stream.JsonParsingException;
-import se.narstrom.myr.json.MyrJsonContext;
 
 final class MyrJsonStreamParserTest {
 
 	@Test
 	void invalidNumber() {
-		final MyrJsonContext context = new MyrJsonContext(Map.of());
-		final Reader reader = new StringReader("1e-lol");
-		final MyrJsonStreamParser parser = new MyrJsonStreamParser(reader, context);
+		final JsonParser parser = Json.createParser(new StringReader("1e-lol"));
 		assertThrows(JsonParsingException.class, () -> parser.next());
 	}
 
 	@Test
 	void invalidStringUnicodeEscapeSequence() {
-		final MyrJsonContext context = new MyrJsonContext(Map.of());
-		final Reader reader = new StringReader("\"\\uloli");
-		final MyrJsonStreamParser parser = new MyrJsonStreamParser(reader, context);
+		final JsonParser parser = Json.createParser(new StringReader("\"\\uloli"));
 		assertThrows(JsonParsingException.class, () -> parser.next());
 	}
 
 	@FuzzTest
 	void fuzzTest(final FuzzedDataProvider provider) {
-		final MyrJsonContext context = new MyrJsonContext(Map.of());
-		final Reader reader = new StringReader(provider.consumeRemainingAsString());
-		try (final MyrJsonStreamParser parser = new MyrJsonStreamParser(reader, context)) {
+		try (final JsonParser parser = Json.createParser(new StringReader(provider.consumeRemainingAsString()))) {
 			while (parser.hasNext())
 				parser.next();
 		} catch (final JsonException ex) {
